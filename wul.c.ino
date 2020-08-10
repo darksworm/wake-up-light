d1d4dda1f115548085a914a005dc97a8b1e9564c
ilmars <9987548+darksworm@users.noreply.github.com>
2020-08-10 22:06:09 +0300

    rewired stuff, eeprom r/w funcs

--- wul.c.ino
+++ wul.c.ino
@@ -3,22 +3,22 @@
 #include "RTClib.h"
 #include <EEPROM.h>
 
-#define LCD_ENABLE A0
-#define LCD_D4 A1
-#define LCD_D5 A2
-#define LCD_D6 A3
-#define LCD_D7 A4
-#define LCD_RS A5
+#define LCD_ENABLE A1
+#define LCD_D4 A2
+#define LCD_D5 A3
+#define LCD_D6 A4
+#define LCD_D7 A5
+#define LCD_RS A0
 #define LCD_BACKLIGHT 7
 
 #define BTN_BLUE 11
 #define BTN_RED 12
-#define BTN_WHITE 8
+#define BTN_WHITE 10
 #define BTN_GREEN 13
 #define BTN_COUNT 4
 
 #define RELAY2 9
-#define RELAY 10
+#define RELAY 8
 #define HIGH1 4
 #define LOW2 6
 #define LOW1 5
@@ -28,11 +28,10 @@
 #define HOUR_END 9
 #define DEBOUNCE_DELAY 200
 
-void turn_on_lcd();
-void turn_off_lcd();
-
-bool button_state_changed(int);
-bool button_is(int, int);
+#define ENABLED_ADDR 0
+#define START_MINUTES_ADDR 1
+#define RAMP_UP_DURATION_MINUTES_ADDR 2
+#define END_MINUTES_ADDR 3
 
 enum class ClockState
 {
@@ -58,6 +57,8 @@ enum Button {
     GREEN = 3
 };
 
+const int buttons[] = {BTN_BLUE, BTN_RED, BTN_WHITE, BTN_GREEN};
+
 struct State {
     ClockState current_clock_state = ClockState::ACTIVE_TIMER;
     ClockVariable current_clock_variable = ClockVariable::DISABLE_CLOCK;
@@ -65,72 +66,98 @@ struct State {
     int prev_button_state[BTN_COUNT] = {0, 0, 0, 0};
     int curr_button_state[BTN_COUNT] = {0, 0, 0, 0};
     int last_debounce_start = millis();
+
+    int start_time_minutes;
+    int ramp_up_duration_minutes;
+    int end_time_minutes;
+    int clock_enabled_temp;
 };
 
-const int buttons[] = {BTN_BLUE, BTN_RED, BTN_WHITE, BTN_GREEN};
+RTC_DS3231 rtc;
+LiquidCrystal lcd(LCD_RS, LCD_ENABLE, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+State state;
+
+const int eeprom_addrs[] = {
+    ENABLED_ADDR,
+    START_MINUTES_ADDR,
+    RAMP_UP_DURATION_MINUTES_ADDR,
+    END_MINUTES_ADDR
+};
+
+int* eeprom_state_variables[] = {
+    &state.clock_enabled_temp,
+    &state.start_time_minutes,
+    &state.ramp_up_duration_minutes,
+    &state.end_time_minutes
+};
+
+int eeprom_default_state_vals[] = { 
+    1,      // enabled
+    7 * 60, // start at 7AM
+    60,     // ramp for 1hr
+    9 * 60  // end at 9AM
+};
 
 int skip_day = -1;
 bool turn_lights_on = false;
 
 const int steps_between = HOUR_FULL - HOUR_START + 1 * 60;
 
-RTC_DS3231 rtc;
-LiquidCrystal lcd(LCD_RS, LCD_ENABLE, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
-State state;
+void turn_on_lcd();
+void turn_off_lcd();
+
+bool button_state_changed(int);
+bool button_is(int, int);
+
+void read_state_from_eeprom(State &state);
+void write_state_to_eeprom(State &state);
+void initialize_default_state_to_eeprom();
+bool eeprom_is_initialized();
 
 void setup () 
 {
-    delay(700);
     Serial.begin(9600);
-    delay(700);
 
-    pinMode(HIGH1, OUTPUT);
-    pinMode(LOW2, OUTPUT);
-    pinMode(LOW1, OUTPUT);
-    pinMode(RELAY, OUTPUT);
-    pinMode(RELAY2, OUTPUT);
-    pinMode(LCD_BACKLIGHT, OUTPUT);
+    if (!eeprom_is_initialized()) {
+        initialize_default_state_to_eeprom();
+    }
+
+    read_state_from_eeprom(state);
+
+    int outputs[] = {HIGH1, LOW2, LOW1, RELAY, RELAY2, LCD_BACKLIGHT};
+    int lowOutputs[] = {HIGH1, LOW2, LOW1, RELAY, RELAY2};
 
     for (int i = 0; i < BTN_COUNT; i++) {
         pinMode(buttons[i], INPUT);
     }
 
-    analogWrite(HIGH1, 0);
-    analogWrite(LOW2, 0);
-    analogWrite(LOW1, 0);
+    for (int i = 0; i < sizeof(outputs) / sizeof(int); i++) {
+        pinMode(outputs[i], OUTPUT);
+    }
 
-    analogWrite(RELAY, 0);
-    analogWrite(RELAY2, 0);
+    for (int i = 0; i < sizeof(lowOutputs) / sizeof(int); i++) {
+        analogWrite(lowOutputs[i], 0);
+    }
 
     lcd.begin(16,2);
 
     if (!rtc.begin()) {
-        while(1) {
-            analogWrite(LED_BUILTIN, 255);
-            delay(750);
-            analogWrite(LED_BUILTIN, 128);
-            delay(750);
-            analogWrite(LED_BUILTIN, 0);
-            delay(750);
-        }
+        turn_on_lcd();
+        lcd.write("RTC can't even!");
+
+        while(1);
     }
 
     if (rtc.lostPower()) {
-        while(1) {
-            analogWrite(LED_BUILTIN, 0);
-            delay(750);
-            analogWrite(LED_BUILTIN, 64);
-            delay(750);
-            analogWrite(LED_BUILTIN, 128);
-            delay(750);
-        }
+        turn_on_lcd();
+        lcd.write("RTC lost power!");
+        // todo: set time here
+
+        while(1);
     }
 
     // Following line sets the RTC to the date & time this sketch was compiled
    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    
-    turn_on_lcd();
-    lcd.write("hello, world!");
 }
 
 void loop () 
@@ -238,4 +265,37 @@ bool button_state_changed(int btn) {
 
 bool button_is(int btn, int val) {
     return state.curr_button_state[btn] == val;
+}
+
+void read_state_from_eeprom(State& state) 
+{
+    for (int i = 0; i < sizeof(eeprom_addrs) / sizeof(int); i++) {
+        *eeprom_state_variables[i] = EEPROM.read(eeprom_addrs[i]);
+    }
+
+    state.current_clock_state = state.clock_enabled_temp ? 
+        ClockState::ACTIVE_TIMER : ClockState::DISABLED;
+}
+
+void write_state_to_eeprom(State& state)
+{
+    state.clock_enabled_temp = state.current_clock_state == ClockState::DISABLED;
+
+    for (int i = 0; i < sizeof(eeprom_addrs) / sizeof(int); i++) {
+        EEPROM.write(eeprom_addrs[i], *eeprom_state_variables[i]);
+    }
+}
+
+void initialize_default_state_to_eeprom()
+{
+    for (int i = 0; i < sizeof(eeprom_addrs) / sizeof(int); i++) {
+        EEPROM.write(eeprom_addrs[i], eeprom_default_state_vals[i]);
+    }
+
+    EEPROM.write(EEPROM.length() - 1, 137);
+}
+
+bool eeprom_is_initialized()
+{
+    return EEPROM.read(EEPROM.length() - 1) == 137;
 }
